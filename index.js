8e2da41454f300e73c6f1e054c1d37d655706449
prabhakar sharma <prabh12435@gmail.com>
2025-11-29 23:53:41 +0530

    kuch bhi

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "https://nexus-frontend-zb37.onrender.com/api",
+    origin: process.env.CLIENT_URL || "https://nexus-frontend-zb37.onrender.com",
     credentials: true,
   }),
 )
